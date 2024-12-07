713e9657e54b528292b2936dc4b583c296f1382b
kim <dmitriy.kim.0508@mail.ru>
2024-12-07 17:00:18 +0300

    2nd commit

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 )
 
 func main() {
-	n := 0
+	var n string
 	fmt.Print("Введите целое число: ")
 	_, err := fmt.Scan(&n)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	var data interface{}
+
+	err = json.Unmarshal([]byte(n), &data)
+	if err != nil {
+		data = n
+	}
+
 	fmt.Printf("Вы ввели число: %d\n", n)
 }
